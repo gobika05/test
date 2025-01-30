630eb65cdd410eaec0c8b295f2b713406a9b9aaf
Gobika S <kopsiko542005@gmail.com>
2025-01-30 14:40:51 +0530

    initial push

---
+++ int.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int num_1;
+    printf("enter your age:");
+    scanf("%u",&num_1);
+    printf("your age is:%u\n",num_1);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+   char var_a;
+    printf("enter any character:");
+    scanf("%c",&var_a);
+    printf("the entered character is:%c",var_a); 
+    return 0;
+}
